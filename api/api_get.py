449f56779adfd3dd0fa1a3e5a8d787813248df02
m <m@mPC.lan>
2023-09-09 19:49:42 +0100

    javascript with py loops

--- api/api_get.py
+++ api/api_get.py
@@ -14,8 +14,10 @@ def categorize_launches(launches):
 
 def categorize_events(launches):
     date_utc = list(x["date_utc"] for x in launches)
+    name = list(x["name"] for x in launches)
     return {
-            "date_utc": date_utc
+            "date_utc": date_utc,
+            "name": name
     }
 def fetch_spacex_launches():
     url = "https://api.spacexdata.com/v5/launches"
